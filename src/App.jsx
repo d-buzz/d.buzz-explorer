6cc6bf906a7c7fb7763f8a54e0fb57234405e2c2
iamjc93 <iamjc93@gmail.com>
2024-02-27 13:31:38 +0800

    config: added react-router-dom and prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setUser } from './redux/actions/userActions';
 
 const App = () => {
-    const user = useSelector((state) => state.user);
+    const { user } = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
     const handleSetUser = () => {
